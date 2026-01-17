1d83877188b64f1a50736adcdad79d99411cf353
Euwen <163235967+Euwenn@users.noreply.github.com>
2026-01-16 18:13:54 -0800

    Delete src directory

--- src/Problem1/MasteringJava100_Problem1_SOLUTION.java
+++
@@ -1,8 +0,0 @@
-package Problem1;
-
-public class MasteringJava100_Problem1_SOLUTION {
-    public static void main (String[] args) {
-        System.out.println("TEST");
-    }
-
-}
--- src/Problem2/MasteringJava100_Problem2_SOLUTION.java
+++
@@ -1,5 +0,0 @@
-package Problem2;
-
-public class MasteringJava100_Problem2_SOLUTION {
-
-}
